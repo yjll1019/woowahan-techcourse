d5a87a5d0c4dbc9869d54d0d612fc941aaf50e70
yejilee <yjll1019@naver.com>
2019-05-21 15:19:34 +0900

    Implement point and distance calculation
    * Edited README.

---
+++ src/main/java/com/woowacourse/coordinate/Point.java
@@ -0,0 +1,78 @@
+package com.woowacourse.coordinate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Point {
+	private static final int MIN = 0;
+	private static final int MAX = 24;
+
+	private int x;
+	private int y;
+
+	public Point(int x, int y) {
+		if (x <= MIN || x > MAX || y <= MIN || y > MAX) {
+			throw new IllegalArgumentException(String.format("Invalid range: %d, %d", x, y));
+		}
+		this.x = x;
+		this.y = y;
+	}
+
+	public Point(String str) {
+		str = str.trim();
+		if (!isSurroundedWithParenthesis(str)) {
+			throw new IllegalArgumentException("Invalid string for create point: " + str);
+		}
+
+		String[] segments = str.split("[,()]");
+		x = Integer.valueOf(segments[1].trim());
+		y = Integer.valueOf(segments[2].trim());
+	}
+
+	public static List<Point> createWithPair(String s) {
+		String[] segments = s.split("\\)[\\s]*-[\\s]*\\(");
+		List<Integer> coords1 = Arrays.asList(segments[0].substring(1).split(","))
+				.stream()
+				.map(String::trim)
+				.map(Integer::valueOf)
+				.collect(Collectors.toList());
+
+		List<Integer> coords2 = Arrays.asList(segments[1].substring(0, segments[1].length() - 1).split(","))
+				.stream()
+				.map(String::trim)
+				.map(Integer::valueOf)
+				.collect(Collectors.toList());
+		return Arrays.asList(new Point(coords1.get(0), coords1.get(1)), new Point(coords2.get(0), coords2.get(1)));
+	}
+
+
+	private boolean isSurroundedWithParenthesis(String s) {
+		return s.startsWith("(") && s.endsWith(")");
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof Point)) {
+			return false;
+		}
+		Point point = (Point) o;
+		return x == point.x &&
+				y == point.y;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
+	}
+
+	public double calculateDistance(Point point) {
+		double dx = Math.abs(point.x - x);
+		double dy = Math.abs(point.y - y);
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+}
---
+++ src/test/java/com/woowacourse/coordinate/PointTest.java
@@ -0,0 +1,51 @@
+package com.woowacourse.coordinate;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PointTest {
+	@Test
+	void creat() {
+		Point point = new Point(10, 10);
+		assertThat(point).isEqualTo(new Point(10, 10));
+	}
+
+	@Test
+	void createWithString() {
+		assertThat(new Point("(4, 10)")).isEqualTo(new Point(4, 10));
+		assertThat(new Point("(4 , 10)")).isEqualTo(new Point(4, 10));
+		assertThat(new Point("( 4 , 10 )")).isEqualTo(new Point(4, 10));
+	}
+
+	@Test
+	void createWithPair() {
+		assertThat(Point.createWithPair("(4, 10)-(13,13)")).containsExactly(new Point(4, 10), new Point(13, 13));
+		assertThat(Point.createWithPair("(4, 10) - (13,13)")).containsExactly(new Point(4, 10), new Point(13, 13));
+		assertThat(Point.createWithPair("(4, 10) - ( 13,13 )")).containsExactly(new Point(4, 10), new Point(13, 13));
+	}
+
+	@Test
+	void validateInvalidString() {
+		assertThrows(IllegalArgumentException.class, () -> new Point("4,10"));
+	}
+
+	@Test
+	void validateRange() {
+		assertThrows(IllegalArgumentException.class, () -> new Point(-1, 10));
+		assertThrows(IllegalArgumentException.class, () -> new Point(10, -1));
+		assertThrows(IllegalArgumentException.class, () -> new Point(5, 25));
+		assertThrows(IllegalArgumentException.class, () -> new Point(25, 5));
+	}
+
+	@Test
+	void calculateDistance() {
+		assertThat(new Point(1, 1).calculateDistance(new Point(1, 3))).isEqualTo(2);
+		assertThat(new Point(1, 1).calculateDistance(new Point(2, 2))).isEqualTo(1.414, offset(0.01));
+	}
+
+}
