891e9ac3e4a0b136605f252265192c4bac7b7091
yejilee <yjll1019@naver.com>
2019-05-22 09:55:49 +0900

    Add Line

---
+++ src/main/java/com/woowacourse/coordinate/Line.java
@@ -0,0 +1,39 @@
+package com.woowacourse.coordinate;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+
+public class Line {
+	public static final int NUM_OF_POINTS = 2;
+
+	private final List<Point> points;
+
+	public Line(List<Point> points) {
+		if(new HashSet<>(points).size() != NUM_OF_POINTS) {
+			throw new IllegalArgumentException("Invalid size of points");
+		}
+		this.points = points;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof Line)) {
+			return false;
+		}
+		Line line = (Line) o;
+		return Objects.equals(points, line.points);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(points);
+	}
+
+	public double calculateLength() {
+		return points.get(0).calculateDistance(points.get(1));
+	}
+}
---
+++ src/test/java/com/woowacourse/coordinate/LineTest.java
@@ -0,0 +1,40 @@
+package com.woowacourse.coordinate;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LineTest {
+
+	@Test
+	void testCreate() {
+		assertThat(new Line(Arrays.asList(new Point(3, 3), new Point(6, 3))))
+				.isEqualTo(new Line(Arrays.asList(new Point(3, 3), new Point(6, 3))));
+	}
+
+	@Test
+	void invalidSizeOfPoints() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			new Line(Arrays.asList(new Point(1, 1)));
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			new Line(Arrays.asList(new Point(1, 1), new Point(5,10), new Point(10, 14)));
+		});
+	}
+
+	@Test
+	void calculateLength() {
+		assertThat(new Line(Arrays.asList(new Point(3, 3), new Point(6, 3))).calculateLength())
+				.isEqualTo(3);
+	}
+
+	@Test
+	void duplicatePoints() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			new Line(Arrays.asList(new Point(5, 10), new Point(5, 10)));
+		});
+	}
+}
