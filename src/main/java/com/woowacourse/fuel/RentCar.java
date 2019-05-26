93be0613093e4eaf25342526343c14d4c67e7958
yejilee <yjll1019@naver.com>
2019-05-26 14:01:38 +0900

    fixed bug

--- src/main/java/com/woowacourse/fuel/RentCar.java
+++ src/main/java/com/woowacourse/fuel/RentCar.java
@@ -1,5 +1,7 @@
 package com.woowacourse.fuel;
 
+import java.util.Objects;
+
 public abstract class RentCar implements Car{
 	private final String name;
 	private final double distancePerLiter;
@@ -19,5 +21,24 @@ public abstract class RentCar implements Car{
 	@Override
 	public double getChargeQuantity() {
 		return distance / distancePerLiter;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof RentCar)) {
+			return false;
+		}
+		final RentCar rentCar = (RentCar) o;
+		return Double.compare(rentCar.distancePerLiter, distancePerLiter) == 0 &&
+				distance == rentCar.distance &&
+				Objects.equals(getName(), rentCar.getName());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getName(), distancePerLiter, distance);
 	}
 }
