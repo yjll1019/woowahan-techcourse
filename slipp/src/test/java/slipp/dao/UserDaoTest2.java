25cd3d09141de8b0604408772d4ea2804a715875
yeji <yjll1019@naver.com>
2019-10-15 18:00:07 +0900

    feat: add UserDao2, uses lambdas

---
+++ slipp/src/test/java/slipp/dao/UserDaoTest2.java
@@ -0,0 +1,47 @@
+package slipp.dao;
+
+import java.util.List;
+
+import nextstep.jdbc.ConnectionManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import slipp.domain.User;
+import slipp.dto.UserUpdatedDto;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserDaoTest2 {
+    @BeforeEach
+    public void setup() {
+        ConnectionManager.initialize("org.h2.Driver", "jdbc:h2:mem:jwp-framework",
+                "sa", "");
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("jwp.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
+    @Test
+    public void crud() {
+        User expected = new User("userId", "password", "name", "javajigi@email.com");
+        UserDao2 userDao = new UserDao2();
+        userDao.insert(expected);
+        User actual = userDao.findByUserId(expected.getUserId());
+        assertThat(actual).isEqualTo(expected);
+
+        expected.update(new UserUpdatedDto("password2", "name2", "sanjigi@email.com"));
+        userDao.update(expected);
+        actual = userDao.findByUserId(expected.getUserId());
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void findAll() {
+        UserDao2 userDao = new UserDao2();
+        List<User> users = userDao.findAll();
+        assertThat(users).hasSize(1);
+    }
+}
