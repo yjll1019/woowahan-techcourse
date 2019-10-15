1a3c5f6ad89134ce7441039596200ce3dd25d3a3
yeji <yjll1019@naver.com>
2019-10-15 16:05:42 +0900

    feat: add JdbcTemplate

--- nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,4 +1,71 @@
 package nextstep.jdbc;
 
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JdbcTemplate {
+    public void updateTemplate(String sql, String... params) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = con.prepareStatement(sql);
+
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+
+            pstmt.executeUpdate();
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+
+    public <T> List<T> selectTemplate(String sql, Class dto, String... params) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = con.prepareStatement(sql);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+
+            rs = pstmt.executeQuery();
+            List<T> result = new ArrayList<>();
+            while (rs.next()) {
+                Object object = dto.getDeclaredConstructor().newInstance();
+                for (Field field : dto.getDeclaredFields()) {
+                    field.setAccessible(true);
+                    field.set(object, rs.getString(field.getName()));
+                }
+                result.add((T) object);
+            }
+            return result;
+        } catch (Exception e) {
+            throw new SelectQueryFailException();
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
 }
---
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/SelectQueryFailException.java
@@ -0,0 +1,7 @@
+package nextstep.jdbc;
+
+public class SelectQueryFailException extends RuntimeException {
+    public SelectQueryFailException() {
+        super("Select 쿼리를 수행하는 데 실패했습니다.");
+    }
+}
