db7400543d054a55d857f57a2631af33f65fb4b0
yeji <yjll1019@naver.com>
2019-10-16 21:33:43 +0900

    refactor: modify field name

--- slipp/src/main/java/slipp/dao/UserDao.java
+++ slipp/src/main/java/slipp/dao/UserDao.java
@@ -7,15 +7,15 @@ import nextstep.jdbc.JdbcTemplate;
 import slipp.domain.User;
 
 public class UserDao {
-    private final JdbcTemplate JDBC_TEMPLATE;
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDao() {
-        this.JDBC_TEMPLATE = new JdbcTemplate();
+        this.jdbcTemplate = new JdbcTemplate();
     }
 
     public void insert(User user) {
         String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-        JDBC_TEMPLATE.updateTemplate(sql, (pstmt) -> {
+        jdbcTemplate.updateTemplate(sql, (pstmt) -> {
             pstmt.setString(1, user.getUserId());
             pstmt.setString(2, user.getPassword());
             pstmt.setString(3, user.getName());
@@ -25,7 +25,7 @@ public class UserDao {
 
     public void update(User user) {
         String sql = "UPDATE USERS SET password=?, name=?, email=? WHERE userId=?";
-        JDBC_TEMPLATE.updateTemplate(sql, (pstmt) -> {
+        jdbcTemplate.updateTemplate(sql, (pstmt) -> {
             pstmt.setString(1, user.getPassword());
             pstmt.setString(2, user.getName());
             pstmt.setString(3, user.getEmail());
@@ -35,7 +35,7 @@ public class UserDao {
 
     public List<User> findAll() {
         String sql = "SELECT userId, password, name, email FROM USERS";
-        return JDBC_TEMPLATE.selectTemplate(sql, ((rs) -> {
+        return jdbcTemplate.selectTemplate(sql, ((rs) -> {
             List<User> users = new ArrayList<>();
             while (rs.next()) {
                 User user = new User(rs.getString("userId"), rs.getString("password"),
@@ -49,7 +49,7 @@ public class UserDao {
 
     public User findByUserId(String userId) {
         String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-        return JDBC_TEMPLATE.selectTemplate(sql, ((rs) -> {
+        return jdbcTemplate.selectTemplate(sql, ((rs) -> {
             List<User> users = new ArrayList<>();
             if (rs.next()) {
                 User user = new User(rs.getString("userId"), rs.getString("password"),
