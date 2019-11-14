14e980fe50b623d57ea72f0ccc8f2883eba571c2
김성훈 <seonghun127@gmail.com>
2019-11-14 13:01:18 +0900

    refactor: separate properties from class

--- nextstep-jdbc/src/main/java/nextstep/jdbc/ConnectionManager.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/ConnectionManager.java
@@ -1,30 +1,52 @@
 package nextstep.jdbc;
 
+import nextstep.jdbc.exception.PropertiesFileLoadFailException;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
-    private static String DRIVER;
-    private static String URL;
-    private static String USERNAME;
-    private static String PASSWORD;
+    private static String driver;
+    private static String url;
+    private static String username;
+    private static String password;
 
-    public static void initialize(String _driver, String url, String userName, String password) {
-        DRIVER = _driver;
-        URL = url;
-        USERNAME = userName;
-        PASSWORD = password;
+    public static void initialize(String _driver, String _url, String _userName, String _password) {
+        driver = _driver;
+        url = _url;
+        username = _userName;
+        password = _password;
+    }
+
+    public static void initialize() {
+        InputStream inputStream = (ConnectionManager.class).getClassLoader()
+                .getResourceAsStream("db.properties");
+
+        Properties properties = new Properties();
+
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            throw new PropertiesFileLoadFailException();
+        }
+
+        driver = properties.getProperty("jdbc.driverClass");
+        url = properties.getProperty("jdbc.url");
+        username = properties.getProperty("jdbc.username");
+        password = properties.getProperty("jdbc.password");
     }
 
     public static DataSource getDataSource() {
         BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName(DRIVER);
-        ds.setUrl(URL);
-        ds.setUsername(USERNAME);
-        ds.setPassword(PASSWORD);
+        ds.setDriverClassName(driver);
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
         return ds;
     }
 
